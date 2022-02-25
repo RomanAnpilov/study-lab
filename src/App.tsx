7dc3246b818a7704d8ad54f467e2fb93eac8203c
RomanAnpilov <rebokuter@gmail.com>
2022-02-25 21:04:23 +0300

    First page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { MathJax, MathJaxContext } from "better-react-mathjax";
+import { MainPage } from "./pages/main";
 
-function App() {
+export default function App() {
+  const config = {
+    loader: { load: ["input/asciimath"] },
+    "fast-preview": { disabled: true },
+    tex2jax: { preview: "none" },
+  };
+  const [set, setSet] = React.useState("123123");
+  const [text, setText] = React.useState("sosiski sosisli");
+
+  const changeHandler = (e: any) => {
+    setSet(e.target.value);
+  };
+
+  console.log(set);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <MathJaxContext renderMode="post" config={config}>
+    //   <input type="text" value={set} onChange={changeHandler} />
+    //   {/* <button onClick={() => setText("sosisj")}>agagagaga</button>
+    //   <h2>{text}</h2> */}
+    //   <MathJax dynamic>{`\`${set}\``}</MathJax>
+    // </MathJaxContext>
+    <MainPage></MainPage>
   );
 }
-
-export default App;
---
+++ src/pages/main.tsx
@@ -0,0 +1,118 @@
+import style from "./main.module.scss";
+import clsx from "clsx";
+
+export const MainPage: React.FC = () => {
+  return (
+    <div className={style.body}>
+      <header>
+        <h1 className={style.logo}>MathAcademy✨</h1>
+        <ul>
+          <li>Курсы</li>
+          <li>Тесты</li>
+          <li>Оценки</li>
+        </ul>
+        <div className={style.avatar}>
+          <img src="IMG_0618.PNG" alt="" />
+          <h2>Роман Анпилов</h2>
+        </div>
+      </header>
+      <div className={clsx(style.mainBlock)}>
+        <div>
+        <h1>Мои тесты</h1>
+        <div className={style.grid}>
+          <div className={style.card}>
+            <h1>Стереометрия</h1>
+            <h3>До - 24.2.2022 15:00🔥</h3>
+            <div className={style.info}>
+              <div>
+                <h2>15 вопросов</h2>
+                <p>10 минут</p>
+              </div>
+
+              <img src="arrow.png" alt="" />
+            </div>
+          </div>
+          <div className={style.card}>
+            <h1>Стереометрия</h1>
+            <h3>До - 24.2.2022 15:00🔥</h3>
+            <div className={style.info}>
+              <div>
+                <h2>15 вопросов</h2>
+                <p>10 минут</p>
+              </div>
+
+              <img src="arrow.png" alt="" />
+            </div>
+          </div>
+          <div className={style.card}>
+            <h1>Стереометрия</h1>
+            <h3>До - 24.2.2022 15:00🔥</h3>
+            <div className={style.info}>
+              <div>
+                <h2>15 вопросов</h2>
+                <p>10 минут</p>
+              </div>
+
+              <img src="arrow.png" alt="" />
+            </div>
+          </div>
+          <div className={style.card}>
+            <h1>Стереометрия</h1>
+            <h3>До - 24.2.2022 15:00🔥</h3>
+            <div className={style.info}>
+              <div>
+                <h2>15 вопросов</h2>
+                <p>10 минут</p>
+              </div>
+
+              <img src="arrow.png" alt="" />
+            </div>
+          </div>
+          <div className={style.card}>
+            <h1>Стереометрия</h1>
+            <h3>До - 24.2.2022 15:00🔥</h3>
+            <div className={style.info}>
+              <div>
+                <h2>15 вопросов</h2>
+                <p>10 минут</p>
+              </div>
+
+              <img src="arrow.png" alt="" />
+            </div>
+          </div>
+          <div className={style.card}>
+            <h1>Стереометрия</h1>
+            <h3>До - 24.2.2022 15:00🔥</h3>
+            <div className={style.info}>
+              <div>
+                <h2>15 вопросов</h2>
+                <p>10 минут</p>
+              </div>
+
+              <img src="arrow.png" alt="" />
+            </div>
+          </div>
+        </div>
+        </div>
+        
+        <div>
+            <h1>Мои оценки</h1>
+            <div className={style.grades}>
+                <div>
+                    <h3>Производная - 22/30 - 4🥇</h3>
+                </div>
+                <div>
+                    <h3>Производная - 22/30 - 4🥇</h3>
+                </div>
+                <div>
+                    <h3>Производная - 22/30 - 4🥇</h3>
+                </div>
+                <div>
+                    <h3>Производная - 22/30 - 4🥇</h3>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+};
