bd28a5c335f173f6983e6e0b29adb72c5b0a9a1e
RomanAnpilov <rebokuter@gmail.com>
2022-04-04 09:16:19 +0300

    Publish

--- src/components/dustbin/index.tsx
+++ src/components/dustbin/index.tsx
@@ -1,40 +1,57 @@
 import { MathJax, MathJaxContext } from "better-react-mathjax";
 import { CSSProperties, FC, useState, useCallback, memo } from "react";
 import { useDrop, DropTargetMonitor } from "react-dnd";
+import { MainContext } from "../../pages/DragDrop";
 import { ItemTypes } from "../ItemTypes";
 import clsx from "clsx";
-import style from "./Dustbin.module.scss"
+import style from "./Dustbin.module.scss";
+import React from "react";
 const styles: CSSProperties = {
   border: "5px solid rgb(0, 128, 128, 0.1)",
   height: "150px",
   width: "150px",
   padding: "2rem",
-  textAlign: "center",
+
+  
 };
 
 export interface TargetBoxProps {
   onDrop: (item: any) => void;
   lastDroppedColor?: any;
   text: string;
-  answer: string;
+  id: string;
 }
 
 const TargetBox: FC<TargetBoxProps> = memo(function TargetBox({
   onDrop,
   lastDroppedColor,
   text,
-  answer
+  id,
 }) {
+  const { ContextData, addAnswer } = React.useContext(MainContext);
   const config = {
     loader: { load: ["input/asciimath"] },
     "fast-preview": { disabled: true },
+    "HTML-CSS": { fonts: [ "sana-Math"], scale: 1000 },
     tex2jax: { preview: "none" },
   };
+
+  const onCli = () => {
+    if(ContextData.length > 0 && ContextData.some(i => i.id === String(id))){
+      console.log("aaa")
+    } else {
+      addAnswer(String(id), lastDroppedColor.name);
+    }
+    
+    console.log(ContextData);
+  }
+
   const [{ isOver, draggingColor, canDrop }, drop] = useDrop(
     () => ({
       accept: [ItemTypes.BOX],
       drop(_item: any, monitor) {
         onDrop(monitor.getItem());
+        // onCli()
         return undefined;
       },
       collect: (monitor: DropTargetMonitor) => ({
@@ -45,15 +62,18 @@ const TargetBox: FC<TargetBoxProps> = memo(function TargetBox({
     }),
     [onDrop]
   );
-  const opacity = isOver ? 1 : 0.7;
+  const opacity = 1;
   let backgroundColor = "#fff";
 
   return (
     <div
       ref={drop}
       data-color={lastDroppedColor || "none"}
-      style={{ ...styles, backgroundColor, opacity,}}
-      className={clsx(lastDroppedColor && (lastDroppedColor.name === answer ? style.check : style.bad))}
+      style={{ ...styles, backgroundColor, opacity }}
+      // className={clsx(
+      //   lastDroppedColor &&
+      //     (lastDroppedColor.name === id ? style.check : style.bad)
+      // )}
       role="TargetBox"
     >
       <MathJaxContext renderMode="post" config={config}>
@@ -61,7 +81,8 @@ const TargetBox: FC<TargetBoxProps> = memo(function TargetBox({
         {!canDrop && lastDroppedColor && (
           <p>
             Answer: <MathJax>{`\`${lastDroppedColor.name}\``}</MathJax>
-            {lastDroppedColor.name === answer ? "GOOD" : "FAIL"}
+            {onCli()}
+            {/* {lastDroppedColor.name === id ? "GOOD" : "FAIL"} */}
           </p>
         )}
       </MathJaxContext>
@@ -72,19 +93,37 @@ const TargetBox: FC<TargetBoxProps> = memo(function TargetBox({
 export interface StatefulTargetBoxState {
   lastDroppedColor: any | null;
 }
-export const Dustbin: FC<{ textProps: string, answerText: string }> = ({ textProps, answerText }) => {
+export const Dustbin: FC<{ textProps: string; id: number }> = ({
+  textProps,
+  id,
+}) => {
+  const { ContextData, addAnswer } = React.useContext(MainContext);
   const [lastDroppedColor, setLastDroppedColor] = useState<any>(null);
   const handleDrop = useCallback(
     (color: any) => setLastDroppedColor(color),
     []
   );
 
+  
+  const onCli = () => {
+    if(ContextData.length > 0 && ContextData.some(i => i.id === String(id))){
+      console.log("aaa")
+    } else {
+      addAnswer(String(id), textProps);
+    }
+    
+    console.log(ContextData);
+  }
+  
+
   return (
+    <>
     <TargetBox
       text={textProps}
-      answer={answerText}
+      id={String(id)}
       lastDroppedColor={lastDroppedColor as any}
       onDrop={handleDrop}
     />
+    </>
   );
 };
--- src/pages/DragDrop/index.tsx
+++ src/pages/DragDrop/index.tsx
@@ -5,10 +5,40 @@ import { Header } from "../../components/Header";
 import axios from "axios";
 import React from "react";
 
+type MainContextProps = {
+  addAnswer: (id: string, answer: string) => void;
+  ContextData: { id: string; answer: string }[];
+};
+
+export const MainContext = React.createContext<MainContextProps>(
+  {} as MainContextProps
+);
+
 export const DragDrop: React.FC = () => {
   const [answers, setAnswers] = React.useState<{ text: string; id: number }[]>(
     []
   );
+  const ContextData: { id: string; answer: string }[] = [];
+
+  const addAnswer = (id: string, answer: string) => {
+    ContextData.push({ id: id, answer: answer });
+  };
+  const [rightAnswers, setRightAnswers] = React.useState(0);
+  const checkAnswer = () => {
+    console.log(ContextData);
+    let counter = 0;
+    for (let i = 0; i < ContextData.length; i++) {
+      console.log(1);
+      if (ContextData[i].answer === questions[i].answer) {
+        counter += 1;
+      }
+    }
+    console.log("aaa");
+    console.log(rightAnswers);
+    setRightAnswers(counter);
+    
+  };
+
   const [questions, setQuestions] = React.useState<
     { text: string; answer: string; id: number }[]
   >([]);
@@ -27,26 +57,32 @@ export const DragDrop: React.FC = () => {
   }, []);
 
   return (
-    <div className={style.page}>
-      <Header />
-      <div className={style.answers}>
-        {answers.map((item) => (
-          <Card name={item.text} />
-        ))}
-      </div>
-      <div className={style.grid}>
-        {questions.map((item) => (
-          <div className={style.cell}>
-            <Dustbin answerText={item.answer} textProps={item.text} />
+    <MainContext.Provider value={{ addAnswer, ContextData }}>
+      {console.log(ContextData)}
+      <div className={style.page}>
+        <Header />
+
+        <div className={style.flex}>
+          <div className={style.answers}>
+            {answers.map((item) => (
+              <Card name={item.text} />
+            ))}
           </div>
-        ))}
-        <div className={style.btn}>
-          <button>
-            Next Card
-            <img src="/arrow.png" />
-          </button>
+          <div className={style.grid}>
+            {questions.map((item) => (
+              <div className={style.cell}>
+                <Dustbin id={item.id} textProps={item.text} />
+              </div>
+            ))}
+            <div className={style.btn}>
+              <button onClick={() => checkAnswer()}>
+                Check answers
+              </button>
+              <p>Right answers {rightAnswers}/ 12</p>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </MainContext.Provider>
   );
 };
